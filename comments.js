5ffb335d35d27a2f94faeb186eda2a0319e2bb8a
George Calderon <gcald@csu.fullerton.edu>
2019-04-19 21:29:58 -0700

    Now fetches comments dynamically

--- comments.js
+++ comments.js
@@ -5,48 +5,52 @@ $(document).ready(function (){
     .then(res => res.json())
     .then(function(data) {
         for (var i = 0; i < data.length; i++) {
+          // Save JSON data for formatting
           var title = data[i].title;
           var body = data[i].body;
           var id = data[i].id;
          
+          // Format JSON file for HTML
           var f_title = `<h2 data-posts="title">${title}</h2>`;
           var f_body = `<p data-posts="body">${body.replace(/\n/g, "<br>")}</p>`;
           var f_id = `<button data-posts="id" value="${id}" type="button">Show comments</button>`;
          
-          $("body").append("<article>");
-          $("body").append(f_title);
-          $("body").append(f_body);
-          $("body").append(f_id);
-          $("body").append(`<section class="comments" id="comments-${id}" hidden><h3>Comments</h3></section></article>`);
-          $("body").append("</article>");
+          // Add formated tags to body
+          $("body").append(`<article>${f_title}${f_body}${f_id}<section class="comments" id="comments-${id}" hidden><h3>Comments</h3></section></article>`);
 
         }
     });
 });
 
 (function (window) {
-  'use strict';
+  $(document).on('click', ':button', function(){
+    var loaded = false;
+    var hidden = true;
+    var btnValue = this.value;
+    if(!loaded){
+      console.log(loaded);
+      loaded = true;
+      fetch(`https://jsonplaceholder.typicode.com/comments?postId=${this.value}`)
+      .then(res => res.json())
+      .then(function(data) {
+        for (var i = 0; i < data.length; i++){
+          var id = data[i].id;
+          var name = data[i].name;
+          var email = data[i].email;
+          var body = data[i].body;
 
-  const BUTTON_SELECTOR = '[data-posts="id"]';
+          var f_body = `<p data-comments="body">${body.replace(/\n/g, "<br>")}</p>`;
+          var f_name = `<address data-comments="name"><a data-comments="email" href="mailto:${email}">${name}</a></address>`;
 
-  let buttons = document.querySelectorAll(BUTTON_SELECTOR);
+          $(`#comments-${btnValue}`).append(`${f_body}${f_name}`);
+        }
+      });
+    }
 
-  buttons.forEach(function (button) {
-    'use strict';
-
-    let sectionSelector = `#comments-${button.value}`;
-    let commentSection = document.querySelector(sectionSelector);
-
-    button.addEventListener('click', function (event) {
-      if (commentSection.hidden) {
-        commentSection.hidden = false;
-        button.textContent = 'Hide comments';
-      } else {
-        commentSection.hidden = true;
-        button.textContent = 'Show comments';
-      }
-
-      event.preventDefault();
-    });
+    if(hidden){
+      $(`#comments-${this.value}`).attr("hidden", false);
+    } else {
+      $(`#comments-${this.value}`).attr("hidden", true);
+    }
   });
 })(window);
